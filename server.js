a5d5cf85b93220b084c232e4289e012753cb9fd0
TikhonovaElena <lenatika0@gmail.com>
2021-06-25 13:52:39 +0300

    changing server js - all rotes to index

--- server.js
+++ server.js
@@ -10,26 +10,37 @@
 
 // console.log('server started '+ port);
 
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+// const express = require('express');
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
 
+// const app = express();
+
+// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// // app.use(staticFileMiddleware);
+
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/index.html'));
+// });
+
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
+
+const express = require('express');
+const port = process.env.PORT || 8080;
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-// app.use(staticFileMiddleware);
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html")
 });
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+app.listen(port);
+console.log('server started!');
