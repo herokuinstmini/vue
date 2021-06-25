2b0bcf429e898b72ea6e0432b53d21cd2745859f
TikhonovaElena <lenatika0@gmail.com>
2021-06-25 11:44:31 +0300

    changing server js

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+// const express = require('express');
+// const path = require('path');
+// const serveStatic = require('serve-static');
+
+// const app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+
+// const port = process.env.PORT || 5000;
+// app.listen(port);
+
+// console.log('server started '+ port);
+
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-console.log('server started '+ port);
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+// app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
